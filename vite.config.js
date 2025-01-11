a0360a458edcb7d19d660acb41b2b1888d7033ea
RectorDev <Rector.dev@gmail.com>
2025-01-11 18:26:55 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rectordev.github.io/alpaca-image-generator/"
+
 });
