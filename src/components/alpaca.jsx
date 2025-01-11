a879354397ec41c7603915d4b57bdb9e0cedc7a0
RectorDev <Rector.dev@gmail.com>
2025-01-11 18:12:52 +0330

    fix / deploy

--- src/components/alpaca.jsx
+++ src/components/alpaca.jsx
@@ -23,39 +23,38 @@ const Alpaca = () => {
     "accessories",
   ];
   const [tab, setTab] = useState();
+  const [backgroundShow, setBackgroundShow] = useState(backgrounds[0].source);
   const handleTab = () => {};
   return (
-    <div className="flex h-fit gap-10  max-lg:flex-col justify-center items-center">
-      <div className="w-fit h-fit relative flex">
-        <img src={backgrounds[0].source} className="-z-10 " alt="background" />
-        <img src={eyes[0].source} className="z-10 absolute" alt="ears" />
-        <img src={null} className="z-10 absolute" alt="" />
-        <img src={ears[0].source} className="z-1 absolute" alt="" />
-        <img src={legs[0].source} className="z-10 absolute" alt="" />
-        <img src={necks[0].source} className="z-1 absolute" alt="" />
-        <img src={mouths[0].source} className="z-10 absolute" alt="" />
-        <img src={hairs[0].source} className="z-10 absolute" alt="" />
+    <div className="flex items-center justify-center gap-10 h-fit max-lg:flex-col">
+      <div className="relative flex w-fit h-fit">
+        <img src={backgroundShow} className="-z-10 " alt="background" />
+        <img src={eyes[0].source} className="absolute z-10" alt="ears" />
+        <img src={null} className="absolute z-10" alt="" />
+        <img src={ears[0].source} className="absolute z-1" alt="" />
+        <img src={legs[0].source} className="absolute z-10" alt="" />
+        <img src={necks[0].source} className="absolute z-1" alt="" />
+        <img src={mouths[0].source} className="absolute z-10" alt="" />
+        <img src={hairs[0].source} className="absolute z-10" alt="" />
       </div>
-      <div className="flex justify-center items-center gap-10 flex-col">
-        <div className="flex gap-2 justify-center flex-wrap h-fit w-fit ">
+      <div className="flex flex-col items-center justify-center gap-10">
+        <div className="flex flex-wrap justify-center gap-2 h-fit w-fit ">
           {tabs.map((item) => (
             <Button
               key={item}
               click={() => {
-              setTab(item);
-
+                setTab(item);
               }}
               text={item}
             />
           ))}
         </div>
-        <div className="flex gap-2 justify-center items-center flex-wrap h-fit w-fit">
+        <div className="flex flex-wrap items-center justify-center gap-2 h-fit w-fit">
           {backgrounds.map((item) => (
             <div key={item.title}>
               <Button
                 click={() => {
-                  setTab(item.title);
-                  console.log(tab);
+                  setBackgroundShow(item.source);
                 }}
                 text={item.title}
               />
